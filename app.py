d6556d97e085f28c900878fc645d0397aa036dc7
Akash <ag308669@gmail.com>
2022-01-07 01:22:56 +0530

    Added all the windows to the PasswordManager class

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from tkinter import *
+from tkinter import ttk
 from theme import *
+from ApplicationWindows import *
 
 
 class PasswordManager(Tk):
@@ -21,40 +23,51 @@ class PasswordManager(Tk):
         self.geometry(f"{app_width}x{app_height}+{int(x)}+{int(y)}")
         self.maxsize(width=app_width, height=app_height)
 
+        # Setting title logo and background color
         self.title("MyPass - PasswordManager")
         self.wm_iconbitmap("Assets/Logo/padlock.ico")
-        self.config(bg=BACKGROUND_COLOR)
+        self.config(bg=BODY_COLOR)
+
+        # Setting the style(theme) of the application
+        self.style = ttk.Style(self)
         self.call("source", "Assets/Theme/proxttk-dark.tcl")
+        self.style.theme_use("proxttk-dark")
 
         # Setting up a default x_padding for the whole application
         self.x_padding = 60
 
         # Now we will create a container for the frames, which itself would be a frame.
-        container = Frame(self, width=app_width, height=app_height, bg=BACKGROUND_COLOR)
+        container = Frame(self, width=app_width, height=app_height, bg=BODY_COLOR)
         container.pack(side=TOP, fill=BOTH, expand=TRUE)
 
         container.grid_rowconfigure(index=0, weight=1)
         container.grid_columnconfigure(index=0, weight=1)
 
-        container.config(bg=BACKGROUND_COLOR)
+        # Creating all the image elements
         self.nav_bar_logo = PhotoImage(file="Assets/Logo/nav_bar_logo.png")
+        self.login_window_image = PhotoImage(file="Assets/Images/login_window.png")
+        self.signup_window_image = PhotoImage(file="Assets/Images/signup_window.png")
+        self.otp_window_image = PhotoImage(file="Assets/Images/otp_window.png")
+        self.reset_window_image = PhotoImage(file="Assets/Images/reset_window.png")
+        self.add_window_image = PhotoImage(file="Assets/Images/add_window.png")
+        self.retrieve_window_image = PhotoImage(file="Assets/Images/retrieve_window.png")
 
         # Now we will create a list of all the windows for our application.
-        self.windows = []
+        self.windows = [LoginWindow, SignupWindow, OtpWindow, ResetWindow, AddWindow, RetrieveWindow]
         self.frames = {}
 
-        # All the windows for our application would be a class inherited from the frame class, which will take two
-        # arguments - parent and controller, and the class PasswordManager is the controller so we will pass "self" as
-        # the controller for all our windows.
+        # All the windows for our application would be a class inherited from the Frame class, which will take two
+        # arguments - parent and controller. The class PasswordManager itself is the controller so we will pass "self"
+        # as the controller for all our windows.
         for F in self.windows:
             frame = F(container, self)
             # In the above line we passed parent as the container and self as the controller.
             self.frames[F] = frame
             frame.grid(row=0, column=0, sticky=NSEW)
 
-    def show_window(self, cont):
+    def show_window(self, window):
         """This function will display the window we want."""
-        frame = self.frames[cont]
+        frame = self.frames[window]
         frame.tkraise()
 
     def run(self):
