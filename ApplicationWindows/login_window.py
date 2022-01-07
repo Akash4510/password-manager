e7ed230667d17a32d6816757f8fbe308855bee74
Akash <ag308669@gmail.com>
2022-01-07 15:43:27 +0530

    Designed the login window and the signup windows

--- ApplicationWindows/login_window.py
+++ ApplicationWindows/login_window.py
@@ -1,5 +1,81 @@
-from tkinter import Frame
+from widgets import *
 
 
-class LoginWindow(Frame):
-    ...
+class LoginWindow(Window):
+    """Login window of the application"""
+
+    def __init__(self, parent, controller):
+        super().__init__(parent, controller)
+        # Inheriting from the Window Class
+
+        # Creating the navigation bar menus
+        self.nav_bar.nav_menu(
+            "SignUp",
+            action=lambda: self.controller.show_signup_window()
+        )
+        self.nav_bar.nav_menu(
+            "Login",
+            action=lambda: self.controller.show_login_window(),
+            is_active=True
+        )
+        self.nav_bar.nav_menu(
+            "About",
+            action=lambda: print("About button clicked")
+        )
+
+        # Adding the image
+        self.add_image(self.controller.login_window_image)
+
+        # Variables for storing inputs
+        self.email = StringVar()
+        self.password = StringVar()
+
+        # Creating input boxes
+        self.email_input = InputBox(
+            self.entry_frame,
+            label="Enter your email: ",
+            var=self.email
+        )
+        self.password_input = InputBox(
+            self.entry_frame,
+            label="Enter your password: ",
+            var=self.password
+        )
+        self.password_input.input_box.config(show="*")
+
+        # Placing the input boxes on the screen
+        self.add_inputs(
+            inputs=[
+                self.email_input,
+                self.password_input
+            ]
+        )
+        self.entry_frame.config(pady=115)
+
+        # Variable for toggling the password visibility
+        self.show_pass_var = IntVar()
+
+        # Checkbox to control the visibility of the password.
+        show_password = ttk.Checkbutton(
+            self.entry_frame,
+            text="Show Password",
+            variable=self.show_pass_var,
+            command=lambda: self.toggle_password_visibility()
+        )
+        show_password.grid(row=4, column=0, pady=(20, 0), sticky=W)
+
+        # Login button
+        self.login_btn = MyButton(
+            self.entry_frame,
+            text="Login",
+            command=lambda: self.controller.show_add_window(),
+        )
+        self.login_btn.grid(row=5, column=0, columnspan=2, pady=(30, 0))
+
+    def toggle_password_visibility(self):
+        """Toggles the visibility of the password input"""
+        value = self.show_pass_var.get()
+        if value == 1:
+            self.password_input.input_box.config(show="")
+        elif value == 0:
+            self.password_input.input_box.config(show="*")
--- ApplicationWindows/signup_window.py
+++ ApplicationWindows/signup_window.py
@@ -1,5 +1,77 @@
-from tkinter import Frame
+from widgets import *
 
 
-class SignupWindow(Frame):
-    ...
+class SignupWindow(Window):
+
+    def __init__(self, parent, controller):
+        super().__init__(parent, controller)
+        # Inheriting from the window class
+
+        # Creating the navigation bar menus
+        self.nav_bar.nav_menu(
+            "SignUp",
+            action=lambda: self.controller.show_signup_window(),
+            is_active=True
+        )
+        self.nav_bar.nav_menu(
+            "Login",
+            action=lambda: self.controller.show_login_window()
+        )
+        self.nav_bar.nav_menu(
+            "About",
+            action=lambda: print("About button clicked")
+        )
+
+        # Adding the image
+        self.add_image(self.controller.signup_window_image)
+
+        # Variables for storing inputs
+        self.name = StringVar()
+        self.email = StringVar()
+        self.password = StringVar()
+        self.confirm_password = StringVar()
+
+        # Creating input boxes
+        self.name_input = InputBox(
+            self.entry_frame,
+            label="Enter your name: ",
+            var=self.name
+        )
+        self.email_input = InputBox(
+            self.entry_frame,
+            label="Enter your email: ",
+            var=self.email
+        )
+        self.password_input = InputBox(
+            self.entry_frame,
+            label="Create a password: ",
+            var=self.password
+        )
+        self.cnf_pass_input = InputBox(
+            self.entry_frame,
+            label="Confirm password: ",
+            var=self.confirm_password
+        )
+
+        self.password_input.input_box.config(show="*")
+
+        # Placing the input boxes on the screen
+        self.add_inputs(
+            inputs=[
+                self.name_input,
+                self.email_input,
+                self.password_input,
+                self.cnf_pass_input
+            ],
+            gap=20
+        )
+        self.entry_frame.config(pady=50)
+
+        # Signup button
+        self.signup_btn = MyButton(
+            self.entry_frame,
+            text="Create account",
+            width=18
+        )
+
+        self.signup_btn.grid(row=8, column=0, columnspan=2, pady=(40, 0))
